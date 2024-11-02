0155ca9076443aea212b5ed81b7baa893864699c
Cedric DJIVO <cedric_dji@yahoo.fr>
2024-11-02 14:03:26 +0100

    Refactor Terraform configuration to use variables for region and CIDR blocks; update README for clarity

--- main.tf
+++ main.tf
@@ -1,36 +1,36 @@
 provider "aws" {
-  region = "us-west-1"
+  region = var.aws_region
 }
 
 resource "aws_vpc" "project-terraform-vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "dev-project-terraform-vpc"
+    Name = "${var.env}-project-terraform-vpc"
   }
 }
 
-resource "aws_subnet" "project-terraform-subnet(public)" {
+resource "aws_subnet" "project-terraform-subnet-public" {
   vpc_id     = aws_vpc.project-terraform-vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.subnet_cidr_block_public
 
-    tags = {
-        Name = "dev-project-terraform-subnet-public"
-    }
+  tags = {
+    Name = "${var.env}-project-terraform-subnet-public"
+  }
 }
 
-resource "aws_subnet" "project-terraform-subnet(private)" {
+resource "aws_subnet" "project-terraform-subnet-private" {
   vpc_id     = aws_vpc.project-terraform-vpc.id
-  cidr_block = "10.0.10.0/24"
+  cidr_block = var.subnet_cidr_block_private
 
-    tags = {
-        Name = "dev-project-terraform-subnet-private"
-    }
+  tags = {
+    Name = "${var.env}-project-terraform-subnet-private"
+  }
 }
 
 output "output_vpc_id" {
   value = aws_vpc.project-terraform-vpc.id
-  
+
 }
